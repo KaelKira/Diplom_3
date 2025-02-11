2cef261d465b7b0624158e095053b7151ffe0f89
Aleksandr Matveev <kaelkira@gmail.com>
2025-02-11 17:28:10 +0400

    multi-browser-added

--- conftest.py
+++ conftest.py
@@ -3,9 +3,14 @@ from selenium import webdriver
 from constants import Constants
 
 
-@pytest.fixture
-def driver():
-    browser = webdriver.Chrome()
+@pytest.fixture(params=['chrome', 'firefox'])
+def driver(request):
+    if request.param == 'chrome':
+        browser = webdriver.Chrome()
+    elif  request.param == 'firefox':
+        browser = webdriver.Firefox()
+    else:
+        raise ValueError('Unknown browser type')
     browser.get(Constants.MAIN_URL)
     yield browser
     browser.quit()
